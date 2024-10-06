1bc10e26cf37ec31577dce9034d88bdc05d23768
sanasla <sana.slama2017@gmail.com>
2024-10-06 23:00:39 +0200

    test 1 smoke test fini et debut test 2

--- cypress/e2e/smocktest.cy.js
+++ cypress/e2e/smocktest.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('Visits the Kitchen Sink', () => {
+describe('smoke test_test5', () => {
+  it('vérifiez la présence des champs et boutons de connexion ', () => {
     cy.visit('http://localhost:8080/#/')
 
      // Find the connexion button and click it
@@ -7,6 +7,31 @@ describe('template spec', () => {
     
      // assertions here to verify the action
      cy.url().should('include', '/login')
+      // Vérifier la présence du champ email
+    cy.get('#username') // Remplace par l'id de ton champ email
+    .should('be.visible');
+     // Vérifier la présence du champ mot de passe
+    cy.get('#password') // Remplace par l'id de ton champ mot de passe
+    .should('be.visible');
+
+  // Vérifier la présence du bouton de soumission
+  cy.get('[data-cy="login-submit"]')
+      .should('be.visible')
 
   })
 })
+
+it(" TEST_2 Vérifier la présence 'd'ajouter au panier' et la disponibilitée du produit login ok", () => {
+  // Connection avec Email et MdP
+  cy.visit('http://localhost:8080/#/')
+
+  // Find the connexion button and click it
+  cy.get('a').contains('Connexion').click()
+  cy.getBySel("login-input-username").type("test2@test.fr");
+    cy.getBySel("login-input-password").type("testtest");
+  
+  
+  
+ 
+  
+});
