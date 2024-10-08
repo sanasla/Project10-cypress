60c91eca9574edfa29ee58250e8312f195309232
sanasla <sana.slama2017@gmail.com>
2024-10-08 23:32:27 +0200

    smoke test changes

--- cypress/e2e/smocktest.cy.js
+++ cypress/e2e/smocktest.cy.js
@@ -3,35 +3,38 @@ describe('smoke test_test5', () => {
     cy.visit('http://localhost:8080/#/')
 
      // Find the connexion button and click it
-     cy.get('a').contains('Connexion').click()
+ 
+     cy.getBySel("nav-link-login").click(); // Clic "connection nav-link"
     
      // assertions here to verify the action
      cy.url().should('include', '/login')
       // Vérifier la présence du champ email
-    cy.get('#username') // Remplace par l'id de ton champ email
-    .should('be.visible');
+      cy.getBySel("login-input-username").should("be.visible");
+    
      // Vérifier la présence du champ mot de passe
-    cy.get('#password') // Remplace par l'id de ton champ mot de passe
-    .should('be.visible');
+     cy.getBySel("login-input-password").should("be.visible");
 
   // Vérifier la présence du bouton de soumission
-  cy.get('[data-cy="login-submit"]')
-      .should('be.visible')
+      cy.getBySel("login-submit").should("be.visible");
 
   })
 })
 
-it(" TEST_2 Vérifier la présence 'd'ajouter au panier' et la disponibilitée du produit login ok", () => {
+it(" TEST_2 Vérifier la présence button d'ajouter au panier' et la disponibilitée du produit login ok", () => {
   // Connection avec Email et MdP
   cy.visit('http://localhost:8080/#/')
-
-  // Find the connexion button and click it
-  cy.get('a').contains('Connexion').click()
+  cy.getBySel("nav-link-login").click();
   cy.getBySel("login-input-username").type("test2@test.fr");
-    cy.getBySel("login-input-password").type("testtest");
+  cy.getBySel("login-input-password").type("testtest");
+  cy.getBySel("login-submit").click();
+  cy.contains("Mon panier").should("be.visible");//" le bouton "Mon panier" dans la place de "Connexion"
   
-  
-  
- 
-  
+
+ // Sélectionner un produit depuis la page "Produits"
+ cy.getBySel("nav-link-products").click();
+ cy.get("button").eq(0).should("contain", "Consulter").click();
+
+  // Vérifier la présence bouton "ajouter au panier" et du champ de disponibilité du produit.
+
+
 });
