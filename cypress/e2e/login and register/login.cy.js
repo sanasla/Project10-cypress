2320869889b3d5e21958a4c22181dd45d3ec1404
sanasla <sana.slama2017@gmail.com>
2024-10-16 22:29:01 +0200

    test register finish

--- cypress/e2e/login and register/login.cy.js
+++ cypress/e2e/login and register/login.cy.js
@@ -23,6 +23,24 @@ describe('login', () => {
       cy.contains("Merci de remplir correctement tous les champs").should("be.visible");
   });
 
+  it("enter an incorrect password", () => {
+    cy.visit("");
+    cy.getBySel("nav-link-login").click();
+    cy.getBySel("login-input-username").type("test2@test.fr");
+    cy.getBySel("login-input-password").type("123466lp");
+    cy.getBySel("login-submit").click();
+    cy.contains("Identifiants incorrects").should("be.visible");
+  });
+  
+  it("enter an mail et mot passe valides", () => {
+    cy.visit("");
+    cy.getBySel("nav-link-login").click();
+    cy.getBySel("login-input-username").type("username");
+    cy.getBySel("login-input-password").type(" password");
+    cy.getBySel("login-submit").click();
+
+  });
+  
 
 
 })
--- cypress/e2e/login and register/register.cy.js
+++ cypress/e2e/login and register/register.cy.js
@@ -1,5 +1,92 @@
+import { faker } from "@faker-js/faker";
+const fakeLastName = faker.person.lastName();
+const fakeFirstName = faker.person.firstName();
+const fakeEmail = faker.internet.email();
+const fakePassword = faker.internet.password({ length: 20 });
+
+
+
 describe("S'inscrire", () => {
 
-    
+    it("TEST 1_Inscription reussi  nouvel utilisateur ", () => {
+        cy.visit("http://localhost:8080/#/register");
+        cy.getBySel("register-input-lastname").type(fakeLastName);
+        cy.getBySel("register-input-firstname").type(fakeFirstName);
+        cy.getBySel("register-input-email").type(fakeEmail);
+        cy.getBySel("register-input-password").type(fakePassword);
+        cy.getBySel("register-input-password-confirm").type(fakePassword);
+        cy.getBySel("register-submit").click();
+        cy.getBySel("nav-link-cart")
+          .should("be.visible")
+          .should("contain", "Mon panier");
+        cy.getBySel("nav-link-logout")
+          .should("be.visible")
+          .should("contain", "Déconnexion");
+      });
 
-});
+     
+    it("TEST 2_Inscription sans remplir champ nom ", () => {
+        cy.visit("http://localhost:8080/#/register");
+        cy.getBySel("register-input-firstname").type(fakeFirstName);
+        cy.getBySel("register-input-email").type(fakeEmail);
+        cy.getBySel("register-input-password").type(fakePassword);
+        cy.getBySel("register-input-password-confirm").type(fakePassword);
+        cy.getBySel("register-submit").click();
+        cy.getBySel("register-errors")
+        .should("be.visible")
+        .should("contain", "Merci de remplir correctement");
+
+    });
+
+
+    it("TEST 3_Inscription sans remplir email ", () => {
+            cy.visit("http://localhost:8080/#/register");
+            cy.getBySel("register-input-lastname").type(fakeLastName);
+            cy.getBySel("register-input-firstname").type(fakeFirstName);
+            cy.getBySel("register-input-password").type(fakePassword);
+            cy.getBySel("register-input-password-confirm").type(fakePassword);
+            cy.getBySel("register-submit").click();
+            cy.getBySel("register-errors")
+            .should("be.visible")
+            .should("contain", "Merci de remplir correctement");
+
+        });
+
+    it("TEST 2_Inscription sans password confirm ", () => {
+            cy.visit("http://localhost:8080/#/register");
+            cy.getBySel("register-input-lastname").type(fakeLastName);
+            cy.getBySel("register-input-firstname").type(fakeFirstName);
+            cy.getBySel("register-input-password").type(fakePassword);
+            cy.getBySel("register-submit").click();
+            cy.getBySel("register-errors")
+            .should("be.visible")
+            .should("contain", "Merci de remplir correctement");
+
+        });
+    
+   
+
+
+    });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
